4eec72e8cfe5b6bb19b14b78392897150854bc74
darshanv_223 <darshanvaghasiya2234@gmail.com>
2024-03-30 17:26:34 +0530

    tictactoe

--- src/App.js
+++ src/App.js
@@ -30,22 +30,27 @@ function Board({xIsNext, squares, onPlay}){
     status = "Next player:" + (xIsNext ? "X" : "O");
   }
   return <>
+    <h1 className="name">Tic Tac Toe</h1>
     <div className="status">{status}</div>
-    <div className="row">
-      <Square value={squares[0]} onSquareClick={()=>handleClick(0)} />
-      <Square value={squares[1]} onSquareClick={()=>handleClick(1)} />
-      <Square value={squares[2]} onSquareClick={()=>handleClick(2)} />
+
+    <div className="container">
+      <div className="row">
+        <Square value={squares[0]} onSquareClick={()=>handleClick(0)} />
+        <Square value={squares[1]} onSquareClick={()=>handleClick(1)} />
+        <Square value={squares[2]} onSquareClick={()=>handleClick(2)} />
+      </div>
+      <div className="row">
+        <Square value={squares[3]} onSquareClick={()=>handleClick(3)} />
+        <Square value={squares[4]} onSquareClick={()=>handleClick(4)} />
+        <Square value={squares[5]} onSquareClick={()=>handleClick(5)} />
+      </div>
+      <div className="row">
+        <Square value={squares[6]} onSquareClick={()=>handleClick(6)} />
+        <Square value={squares[7]} onSquareClick={()=>handleClick(7)} />
+        <Square value={squares[8]} onSquareClick={()=>handleClick(8)} />
+      </div>
     </div>
-    <div className="row">
-      <Square value={squares[3]} onSquareClick={()=>handleClick(3)} />
-      <Square value={squares[4]} onSquareClick={()=>handleClick(4)} />
-      <Square value={squares[5]} onSquareClick={()=>handleClick(5)} />
-    </div>
-    <div className="row">
-      <Square value={squares[6]} onSquareClick={()=>handleClick(6)} />
-      <Square value={squares[7]} onSquareClick={()=>handleClick(7)} />
-      <Square value={squares[8]} onSquareClick={()=>handleClick(8)} />
-    </div>
+    
   </>
 }
 
@@ -92,6 +97,8 @@ export default function Game(){
     </>
   );
 }
+
+
 
 function decideWinner(squares){
   const lines = [
